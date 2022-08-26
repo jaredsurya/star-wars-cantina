b8d0670bd947c58f713c9c3766fab4af1deb31f2
Jared Dietz <jaredsurya@gmail.com>
2022-08-26 17:07:33 -0400

    populated form queries. needs submit button

--- star-wars-cantina/src/components/CharacterList.js
+++ star-wars-cantina/src/components/CharacterList.js
@@ -1,4 +1,5 @@
-
+import React from "react"
+import Character from "./Character";
 
 function CharacterList() {
   return (
--- star-wars-cantina/src/components/Header.js
+++ star-wars-cantina/src/components/Header.js
@@ -1,7 +1,13 @@
+import React from "react"
 
 
 function Header() {
-  
+  return (
+    <div className="header">
+      <h1>STAR WARS Cantina</h1>
+      <h3>Find your favorite character below.</h3>
+    </div>
+  )
 }
 
 export default Header
