fce803e0b597073f2cf3ce5fd56221b5cad03913
Jared Dietz <jaredsurya@gmail.com>
2022-09-01 18:21:58 -0400

    adding search function

--- star-wars-cantina/src/components/CharacterList.js
+++ star-wars-cantina/src/components/CharacterList.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useState} from "react";
 import Character from "./Character";
 import { Link } from "react-router-dom";
 
-function CharacterList({ characters, onDelete }) {
+function CharacterList({ characters, onDelete, onSearch }) {
+  const [search, setSearch] = useState('')
+  
   const charList = characters.map((character) => (
     <Character character={character} onDelete={onDelete} key={character.id} />
   ));
+  function handleSearch(e) {
+    e.preventDefault()
+    return null
+  }
+  
   return (
     <div className="App">
       <Link className="links" to="/">
         Home Page
       </Link>
+      <form onSubmit={handleSearch}>
+        <input
+          type="text"
+          name="name"
+          placeholder="Search by name:"
+          className="input-text"
+          onChange={(e) => setSearch(e.target.value)}
+        />
+        <input type="submit" className="submit" />
+      </form>
       {charList}
       <div className="navlink">
         <Link className="links" to="/">
