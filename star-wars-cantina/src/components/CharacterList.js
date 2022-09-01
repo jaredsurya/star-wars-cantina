87561cbcbd69a5a7a2ab5012af23ec7a62fbcc3d
Jared Dietz <jaredsurya@gmail.com>
2022-09-01 19:16:20 -0400

    search function works

--- star-wars-cantina/src/components/CharacterList.js
+++ star-wars-cantina/src/components/CharacterList.js
@@ -4,34 +4,29 @@ import { Link } from "react-router-dom";
 
 function CharacterList({ characters, onDelete, onSearch }) {
   const [search, setSearch] = useState('')
-  //console.log(search)
+  
   const filteredChar = characters.filter((character) => {
-    character.name.toLowerCase().includes(search.toLowerCase)
+    let lowercase = character.name.toLowerCase()
+    return lowercase.includes(search)
   })
-  console.log(filteredChar)
-  const charList = characters.map((character) => (
+  
+  const charList = filteredChar.map((character) => (
     <Character character={character} onDelete={onDelete} key={character.id} />
   ));
-  function handleSearch(e) {
-    e.preventDefault()
-    console.log(search)
-    e.target.reset()
-  }
   
   return (
     <div className="App">
       <Link className="links" to="/">
         Home Page
       </Link>
-      <form onSubmit={handleSearch}>
+      <form>
         <input
           type="text"
           name="name"
-          placeholder="Search by name:"
+          placeholder="Type here to search."
           className="input-text"
           onChange={(e) => setSearch(e.target.value.toLowerCase())}
         />
-        <input type="submit" className="submit" />
       </form>
       {charList}
       <div className="navlink">
